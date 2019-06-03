38d68feb55667f0775bb04ab784f9e30f0669f0d
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2019-06-03 21:53:43 +0800

    Add section data and model in form and db

--- ikp_sis/models.py
+++ ikp_sis/models.py
@@ -10,6 +10,8 @@ GENDER_CHOICES = [
 LEVEL_CHOICES = [
     ('None', 'Select Level'),
     ('Kindergarten', 'Kindergarten'),
+    ('Kindergarten A', 'Kindergarten A'),
+    ('Kindergarten B', 'Kindergarten B'),
     ('SPED', 'SPED'),
     ('Alternative Reading Class', 'Alternative Reading Class'),
     ('Alternative Tutorial Class', 'Alternative Tutorial Class'),
@@ -20,11 +22,23 @@ LEVEL_CHOICES = [
     ('Grade 5', 'Grade 5'),
     ('Grade 6', 'Grade 6'),
     ('Grade 7', 'Grade 7'),
+    ('Grade 7 (ALS)', 'Grade 7 (ALS)'),
+    ('Grade 7 (Open High)', 'Grade 7 (Open High)'),
     ('Grade 8', 'Grade 8'),
+    ('Grade 8 (ALS)', 'Grade 8 (ALS)'),
+    ('Grade 8 (Open High)', 'Grade 8 (Open High)'),
     ('Grade 9', 'Grade 9'),
+    ('Grade 9 (ALS)', 'Grade 9 (ALS)'),
+    ('Grade 9 (Open High)', 'Grade 9 (Open High)'),
     ('Grade 10', 'Grade 10'),
+    ('Grade 10 (ALS)', 'Grade 10 (ALS)'),
+    ('Grade 10 (Open High)', 'Grade 10 (Open High)'),
     ('Grade 11', 'Grade 11'),
+    ('Grade 11 (ALS)', 'Grade 11 (ALS)'),
+    ('Grade 11 (Open High)', 'Grade 11 (Open High)'),
     ('Grade 12', 'Grade 12'),
+    ('Grade 12 (ALS)', 'Grade 12 (ALS)'),
+    ('Grade 12 (Open High)', 'Grade 12 (Open High)'),
     ('First Year College', 'First Year College'),
     ('Second Year College', 'Second Year College'),
     ('Third Year College', 'Third Year College'),
@@ -36,8 +50,16 @@ PROMOTION_CHOICES = [
     ('Graduated', 'Graduated'),
     ('Waiting for Accreditation and Equivalency Test Result',
      'Waiting for Accreditation and Equivalency Test Result'),
+    ('Recommended for Philippine Educational Placement Test',
+     'Recommended for Philippine Educational Placement Test'),
     ('Retained', 'Retained'),
-    ('Retained, Inactive', 'Retained, Inactive'),
+    ('Retained; Active', 'Retained; Active'),
+    ('Retained; Inactive', 'Retained; Inactive'),
+    ('Promoted', 'Promoted'),
+    ('Promoted (Status Quo)', 'Promoted (Status Quo)'),
+    ('Promoted to Kindergarten', 'Promoted to Kindergarten'),
+    ('Promoted to Kindergarten A', 'Promoted to Kindergarten B'),
+    ('Promoted to Kindergarten A', 'Promoted to Kindergarten B'),
     ('Promoted to Grade 1', 'Promoted to Grade 1'),
     ('Promoted to Grade 2', 'Promoted to Grade 2'),
     ('Promoted to Grade 3', 'Promoted to Grade 3'),
@@ -56,6 +78,10 @@ PROMOTION_CHOICES = [
     ('Promoted to Fourth Year College', 'Promoted to Fourth Year College'),
 ]
 
+COURSE_CHOICES = [
+    ('None', 'None')
+]
+
 
 class StudentInfo(models.Model):
     profile_image = models.ImageField(
@@ -67,6 +93,9 @@ class StudentInfo(models.Model):
         max_length=100, choices=GENDER_CHOICES, default='None')
     level = models.CharField(
         max_length=100, default='None', choices=LEVEL_CHOICES)
+    section = models.CharField(max_length=100, default='Not Applicable')
+    course = models.CharField(
+        max_length=100, default='None', choices=COURSE_CHOICES)
     school_last_attended = models.CharField(max_length=200)
     school_current = models.CharField(max_length=200)
     date_ikp_enrollment = models.DateField()
