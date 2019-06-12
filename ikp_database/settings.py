b8b25e488aac70fea684906ed5e022d766a28b32
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2019-06-12 21:48:02 +0800

    Fix whitenoise deprecation issue

--- ikp_database/settings.py
+++ ikp_database/settings.py
@@ -133,4 +133,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
