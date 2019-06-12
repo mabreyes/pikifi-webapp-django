33eef935c7dd55c3f91d32a441cfd3160c2cc43c
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2019-06-13 00:00:36 +0800

    Remove expiring password middleware

--- ikp_database/settings.py
+++ ikp_database/settings.py
@@ -28,10 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-# seconds until pw expires, this example shows five days
-ACCOUNT_PASSWORD_EXPIRY = 60*60*24*5
-ACCOUNT_PASSWORD_USE_HISTORY = True
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -45,7 +41,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'account.middleware.ExpiredPasswordMiddleware',
 ]
 
 ROOT_URLCONF = 'ikp_database.urls'
@@ -137,4 +131,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
