4fbf4793302c799d5cc41cecd92183068d295f74
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2019-06-13 00:02:33 +0800

    Uncomments whitenoise storage

--- ikp_database/settings.py
+++ ikp_database/settings.py
@@ -131,4 +131,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
