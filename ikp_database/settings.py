184e7ae323550ce2f4345105ef919dcdea58b34b
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2019-06-12 21:41:28 +0800

    set STATICFILES_STORAGE

--- ikp_database/settings.py
+++ ikp_database/settings.py
@@ -132,3 +132,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
