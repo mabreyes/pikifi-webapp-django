518c53541ea92cd8f11c97378a0e2b96207935c2
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2019-07-15 20:48:52 +0800

    Register users to admin

--- ikp_sis/admin.py
+++ ikp_sis/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import StudentInfo
+from .models import StudentInfo, DatabaseUser
 # Register your models here.
 
 admin.site.register(StudentInfo)
+admin.site.register(DatabaseUser)
