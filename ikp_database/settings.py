e9504a4dd7a4127728d7278d88bb2bf856d96758
Marc Anthony Reyes <marc.reyes@mynt.xyz>
2020-09-25 22:47:04 +0800

    Update length of form and settings

--- ikp_database/settings.py
+++ ikp_database/settings.py
@@ -29,6 +29,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['pikifi.marcreyes.ph',
                  'pikifi-students.herokuapp.com',
+                 'pikifi-db.herokuapp.com',
                  'marcx',
                  'localhost',
                  '127.0.0.1']
